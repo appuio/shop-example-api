34fb4819f7c8b7bc062c895fca3c828c674ae227
Roland Schläfli <rolandschlaefli@gmail.com>
2017-02-23 11:36:48 +0100

    Add additional data to the API

--- app/v1/controllers/ProductsController.scala
+++ app/v1/controllers/ProductsController.scala
@@ -6,7 +6,7 @@ import play.api.db.slick.DatabaseConfigProvider
 import play.api.libs.json._
 import play.api.mvc._
 import slick.driver.JdbcProfile
-import v1.models.{Product, Products}
+import v1.models.{Category, Categories, Language, Languages, LicenseType, LicenseTypes, ProductLanguages, Product, Products, Publisher, Publishers}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -16,31 +16,64 @@ class ProductsController @Inject()(dbConfigProvider: DatabaseConfigProvider) ext
   import dbConfig.driver.api._
 
   private val products = TableQuery[Products]
+  private val categories = TableQuery[Categories]
+  private val licenseTypes = TableQuery[LicenseTypes]
+  private val publishers = TableQuery[Publishers]
 
+  // implicit formats for the case classes
+  // generates mapping of case class to JSON and reverse
   implicit val productFormat: Format[Product] = Json.format[Product]
+  implicit val categoryFormat: Format[Category] = Json.format[Category]
+  implicit val licenseTypeFormat: Format[LicenseType] = Json.format[LicenseType]
+  implicit val publisherFormat: Format[Publisher] = Json.format[Publisher]
 
   def index = Action.async {
-    val allProductsQuery = products.result
+    val allProductsQuery = for {
+      p <- products
+      c <- categories
+      l <- licenseTypes
+      pb <- publishers
+    } yield (p, c, l, pb)
 
     for {
-      result <- dbConfig.db.run(allProductsQuery)
+      result <- dbConfig.db.run(allProductsQuery.result)
     } yield {
       Ok(Json.obj(
         "success" -> true,
-        "items" -> Json.toJson(result)
+        "items" -> result.map {
+          row =>
+            Json.obj(
+              "product" -> row._1,
+              "category" -> row._2,
+              "licenseType" -> row._3,
+              "publisher" -> row._4
+            )
+        }
       ))
     }
   }
 
   def read(id: Int) = Action.async {
-    val productQuery = products.filter(_.id === id).result.head
+    val productQuery = products.filter(_.id === id)
+
+    val extendedProductQuery = for {
+      p <- productQuery
+      c <- categories
+      l <- licenseTypes
+      pb <- publishers
+    } yield (p, c, l, pb)
 
     for {
-      result <- dbConfig.db.run(productQuery)
+      result <- dbConfig.db.run(extendedProductQuery.result.head)
     } yield {
       Ok(Json.obj(
         "success" -> true,
-        "data" -> Json.toJson(result)
+        "data" -> Json.obj(
+          "product" -> result._1,
+          "category" -> result._2,
+          "licenseType" -> result._3,
+          "publisher" -> result._4
+        )
       ))
     }
   }
