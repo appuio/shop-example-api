d63b1d30e1098dabe195b10923f7cbf86409ae4e
Roland Schläfli <rolandschlaefli@gmail.com>
2017-04-12 14:56:51 +0200

    Remove entrypoint

--- start-build.sh
+++ start-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build ../../s2i/play -t scala-play-s2i
-s2i build --loglevel 5 -s image:///usr/libexec/s2i . scala-play-s2i shop-example-api
+s2i build --incremental=true --loglevel 5 -s image:///usr/libexec/s2i . scala-play-s2i shop-example-api
